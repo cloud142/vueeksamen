d9bd9eab05c589a0d0385b39ebd964f79e7e14a8
kingsalle <salma2009@live.dk>
2023-12-04 14:59:05 +0100

    hej

---
+++ Appp.vue
@@ -0,0 +1,23 @@
+<template>
+	
+<Salma/>
+  <router-view />
+
+</template>
+
+<script>
+import Salma from './components/Salma.vue';
+export default {
+components: { Salma },
+name: "Appp",
+components: {
+Salma,  
+}
+
+};
+
+</script>
+
+<style lang="scss">
+
+</style>
